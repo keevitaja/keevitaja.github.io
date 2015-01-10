7c65bd5f33350b9af7c81c287fe7cb1ac2c0e082
Tanel Tammik <keevitaja@gmail.com>
2015-01-10 19:36:11 +0200

    tram

---
+++ public/index.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="et">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+
+        <link href="/lightbox/css/lightbox.css" rel="stylesheet" />
+        <link rel="stylesheet" href="app.css">
+    </head>
+    <body>
+        <div class="wrap">
+            <div class="main-header">
+                <div class="side-margins">
+                    <div class="container">
+                        <a class="logo" href="/"><img src="logo.png"></a>
+
+                        <ul class="main-nav">
+                            <li><a href="/">TÖÖD</a></li>
+                            <li><a href="/">KONTAKT</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="front">
+                <div class="slogan">
+                    <div class="side-margins">
+                        <div class="container">
+                            <h1>TRAM OSKAB<br> EHITADA HOONEID</h1>
+                            <h2>SIIA TULEB KA MINGI TEXT</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="columns">
+                    <div class="side-margins">
+                        <div class="container">
+                            <div class="column">
+                                <h3>ELAMU EHITUS</h3>
+                                Lorem ipsum dolor sit amet, conse adipis elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco.
+                            </div>
+                            <div class="column">
+                                <h3>KORTERITE REMONT</h3>
+                                Lorem ipsum dolor sit amet, conse adipis elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco.
+                            </div>
+                            <div class="column">
+                                <h3>PEAPARANDUS</h3>
+                                Lorem ipsum dolor sit amet, conse adipis elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="works">
+                    <div class="side-margins">
+                        <div class="container">
+                            <h3>MEIE EHITATUD ASJAD</h3>
+
+                            <div class="images">
+                                <a class="item" data-lightbox="works" href="work-large.jpg"><img src="work.jpg"></a>
+                                <a class="item" data-lightbox="works" href="work-large.jpg"><img src="work.jpg"></a>
+                                <a class="item" data-lightbox="works" href="work-large.jpg"><img src="work.jpg"></a>
+                                <a class="item" data-lightbox="works" href="work-large.jpg"><img src="work.jpg"></a>
+                                <a class="item" data-lightbox="works" href="work-large.jpg"><img src="work.jpg"></a>
+                                <a class="item" data-lightbox="works" href="work-large.jpg"><img src="work.jpg"></a>
+                            </div>
+
+                            <a class="more" href="/">VAATA LISAKS</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="main">
+                
+            </div>
+
+            <div class="main-footer">
+                <div class="side-margins">
+                    <div class="container">
+                        <div class="left">Trami Ehitus OÜ</div>
+                        <div class="right">Telefon +372 555 5555, Email <a href="mailto:tram@tram.com">tram@tram.com</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="/lightbox/js/lightbox.min.js"></script>
+        <script src="/app.js"></script>
+    </body>
+</html>
